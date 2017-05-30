f6cad96638bc81e96c999418af8f5d5de77de9aa
Roman Popat <romanpopat@gmx.com>
2017-05-30 12:36:44 +0100

    skeleton documentation

---
+++ R/sim16_domains.R
@@ -0,0 +1,15 @@
+#' Domain ranks from published SIMD 2016
+#'
+#' [describe it here]
+#'
+#' Please see the technical notes \url{http://www.gov.scot/Resource/0050/00504822.pdf} for full information on all of the data in SIMD.
+#'
+#' @format A data frame with ___ rows and ___ variables:
+#' \describe{
+#'   \item{variable}{description}
+#'   \item{}{Count of individuals with no qualifications}
+#'   \item{}{Normalised count, derived from SIMD proceedure in SAS}
+#'   ...
+#' }
+#' @source \url{http://www.gov.scot/Topics/Statistics/SIMD}
+"simd16_domains"
